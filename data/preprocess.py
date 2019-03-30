f4d04bafd2189a0402512545d1ce3c3e2a08b2b8
faqian11 <joannaqian1107@gmail.com>
2019-03-30 14:00:02 -0400

    sort order of images and labels

--- data/preprocess.py
+++ data/preprocess.py
@@ -13,28 +13,42 @@ import numpy as np
 # directory where the data will be saved
 SAVE_DIR = "new_data/"
 
-# save the correspoding labels
-def getLabels (IMG,files):
-    imgLabel = np.zeros(IMG.shape)
-    cnt = 0
-    # sort the label files
+# helper function for sorting filenames
+def sortFilenames(dic, key):
+    files = dic[key]
     order = []
     for f in files:
         f = f[:-4]
         f = int(f.split('ph')[-1])
         order.append(f)
-
+    sorted_files = []
     root = files[0].split('ph')[0] + 'ph'
     for num in sorted(order):
-        f = root+str(num)+'.png'
-        print(f)
-        # convert png to grayscale
-        img = color.rgb2gray(io.imread(f))
-        imgLabel[cnt,:,:]  = img
-        cnt = cnt + 1
+        f = root+str(num)+'.dcm'
+        sorted_files.append(f)
+    return sorted_files
 
-    imgLabel = np.where(imgLabel!=0,1,imgLabel)
-    return imgLabel
+# create a dictionary of series
+# key: series name, ex: SAxx or LAxx
+# value: the sorted filenames for that series
+def createDictionary(directory):
+    series_dict = {}
+    for root, directories, filenames in os.walk(directory):
+        for i in filenames:
+            if i.endswith('.dcm'):
+                splitted = i.split("_")
+                series_name = splitted[0]+"_"+splitted[1]
+                i =  os.path.join(root, i)
+                if series_name in series_dict:
+                    series_dict[series_name].append(i)
+                else:
+                    series_dict[series_name] = []
+                    series_dict[series_name].append(i)
+    for key, value in series_dict.items():
+        sorted_value = sortFilenames(series_dict,key)
+        series_dict[key] = sorted_value
+    return series_dict
+
 
 def getName(dirname):
     name = dirname.split('/')
@@ -42,8 +56,8 @@ def getName(dirname):
     name = ''.join(name.split('_')[0:2])
     return name
 
-# reading labels just in the order of files
-def getLabel_new(IMG,files):
+
+def getLabel_sorted(IMG,files):
     imgLabel = np.zeros(IMG.shape)
     cnt = 0
     for f in files:
@@ -56,16 +70,18 @@ def getLabel_new(IMG,files):
     return imgLabel
 
 
-def processDirectory(dir):
+def processDirectory_sorted(dir):
     print( "Reading Dicom directory:", dir )
     reader = sitk.ImageSeriesReader()
-    series_ids = reader.GetGDCMSeriesIDs(dir)
+    #series_ids = reader.GetGDCMSeriesIDs(dir)
+    series_dict = createDictionary(dir)
+    
     # for each series
-    for i in series_ids:
-        # read the image series
-        dicom_names = reader.GetGDCMSeriesFileNames( dir,i )
+    for key,value in series_dict.items():
+        dicom_names = series_dict[key]
+        dicom_names = tuple(dicom_names)
         reader.SetFileNames(dicom_names)
-        
+
         image = reader.Execute()
 
         name = getName(dicom_names[0])
@@ -76,12 +92,13 @@ def processDirectory(dir):
         spacing = image.GetSpacing()
         origin = image.GetOrigin()
         direction = image.GetDirection()
-        imgLabel = getLabel_new(IMG,dicom_names)
+        imgLabel = getLabel_sorted(IMG,dicom_names)
         imgLabel2 = sitk.GetImageFromArray(imgLabel)
         imgLabel2.SetSpacing(spacing)
         imgLabel2.SetDirection(direction)
         imgLabel2.SetOrigin(origin)
         sitk.WriteImage(imgLabel2, SAVE_DIR+name+"_label.nii")
+
 
 if __name__ == '__main__':
     # create the data directory
@@ -92,7 +109,64 @@ if __name__ == '__main__':
     else:
         print("Successfully created the directory %s" % SAVE_DIR)
     # loop through all the subdirectories
+    count = 0
     for root, directories, filenames in os.walk(sys.argv[1]):
         for d in directories:
-            processDirectory(os.path.join(root,d))
+            processDirectory_sorted(os.path.join(root,d))
+            count += 1
+            if count%10 == 0:
+                print("Processed 10 directories")
+            
+            
+# save the correspoding labels
+# def getLabels (IMG,files):
+#     imgLabel = np.zeros(IMG.shape)
+#     cnt = 0
+#     # sort the label files
+#     order = []
+#     for f in files:
+#         f = f[:-4]
+#         f = int(f.split('ph')[-1])
+#         order.append(f)
+
+#     root = files[0].split('ph')[0] + 'ph'
+#     for num in sorted(order):
+#         f = root+str(num)+'.png'
+#         print(f)
+#         # convert png to grayscale
+#         img = color.rgb2gray(io.imread(f))
+#         imgLabel[cnt,:,:]  = img
+#         cnt = cnt + 1
+
+#     imgLabel = np.where(imgLabel!=0,1,imgLabel)
+#     return imgLabel
+            
+# def processDirectory(dir):
+#     print( "Reading Dicom directory:", dir )
+#     reader = sitk.ImageSeriesReader()
+#     series_ids = reader.GetGDCMSeriesIDs(dir)
+#     # for each series
+#     for i in series_ids:
+#         # read the image series
+#         dicom_names = reader.GetGDCMSeriesFileNames( dir,i )
+#         reader.SetFileNames(dicom_names)
+
+        
+#         image = reader.Execute()
+
+#         name = getName(dicom_names[0])
+#         sitk.WriteImage(image, SAVE_DIR+ name+".nii" )
+#         IMG = sitk.GetArrayFromImage(image)
+
+#         # set label.nii with image metadata
+#         spacing = image.GetSpacing()
+#         origin = image.GetOrigin()
+#         direction = image.GetDirection()
+#         imgLabel = getLabel_new(IMG,dicom_names)
+#         imgLabel2 = sitk.GetImageFromArray(imgLabel)
+#         imgLabel2.SetSpacing(spacing)
+#         imgLabel2.SetDirection(direction)
+#         imgLabel2.SetOrigin(origin)
+#         sitk.WriteImage(imgLabel2, SAVE_DIR+name+"_label.nii")
+
     

ecabf7ddae165e4933eeffdc6bc1217d8f423c96
Jiaming Bai <jiamingbai@Jiaming.local>
2019-04-09 17:48:08 -0400

    add generate_patches

---
+++ data/generate_patches.py
@@ -0,0 +1,30 @@
+from sklearn.feature_extraction import image
+from PIL import Image
+import cv2
+import os
+
+
+def Image_generate_patches(img_dir):
+	for root, directories, filenames in os.walk(img_dir):
+		for filename in filenames:
+			if filename.endswith('.jpg'):
+				dirName = os.path.join(root, filename.split(".jpg")[0])
+				os.mkdir(dirName)
+				filei = os.path.join(root, filename)
+
+				img = cv2.imread(filei)
+				patch_size = 227
+				img_width = img.shape[0]
+				img_height = img.shape[1]
+				stride = 113
+				patches = []
+				for x in range(0, img_width - patch_size, stride):
+					for y in range(0, img_height - patch_size, stride):
+						img_patch = img[x:x+patch_size,y:y+patch_size,:]
+						#patches.append(img_patch)
+						img_patch = Image.fromarray(img_patch, 'RGB')
+						img_patch.save(dirName+"/"+"_x"+str(x)+"_y"+str(y)+".png")
+
+if __name__ == '__main__':
+	Image_generate_patches("./NYC")
+	Image_generate_patches("./PIT")

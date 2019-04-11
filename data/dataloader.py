73cac50b632bb1374080ac897d3ead2d31684e44
Jiaming Bai <jiamingbai@Jiaming.wv.cc.cmu.edu>
2019-04-11 10:44:17 -0400

    add dataloader for pittOrMan

---
+++ data/dataloader.py
@@ -0,0 +1,57 @@
+import os
+import numpy as np
+from PIL import Image
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+class ImageDataset(Dataset):
+    def __init__(self, file_list, target_list):
+        self.file_list = file_list
+        self.target_list = target_list
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        img = Image.open(self.file_list[index])
+        img = torchvision.transforms.ToTensor()(img)
+        label = self.target_list[index]
+        return img, label
+
+#given root directory parse data
+def parse_data(datadir):
+    img_list = []
+    label_list = []
+    for root, directories, filenames in os.walk(datadir):
+        for filename in filenames:
+            if filename.endswith('.png'):
+                filei = os.path.join(root, filename)
+                img_list.append(filei)
+                if "NYC" in filei:
+                	label_list.append(0)
+                else:
+                	label_list.append(1)
+
+    print('{}\t\t{}\n'.format('#Images', len(img_list)))
+    return img_list, label_list
+
+
+def get_loader(mode="train"):
+    if mode == "train":
+        data_path = paths.train_data
+        shuffle = True
+        img_list, label_list = parse_data(data_path)
+        dataset = ImageDataset(img_list, label_list)
+        dataloader = DataLoader(dataset, shuffle=shuffle, batch_size=config.batch_size, drop_last=False)
+    if mode == "val":
+        data_path = paths.valid_data
+        shuffle = False
+        img_list, label_list = parse_data(data_path)
+        dataset = ImageDataset(img_list, label_list)
+        dataloader = DataLoader(dataset, shuffle=shuffle, batch_size=config.batch_size, drop_last=False)
+
+    return dataloader
